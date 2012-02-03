6c812411a0ededff4ce5017bb90ea561455de07f
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2012-02-03 21:38:42 +0100

    added log and error handling to eclib

--- src/eclib.c
+++ src/eclib.c
@@ -1,20 +1,35 @@
 #include "eclib.h"
+#include "logger.h"
+#include <stdlib.h>
 
 static inline void *ec_allocated(void *ptr)
 {
 	if (ptr)
 		return ptr;
 
-	/* TODO: error handling */
-	return NULL;
+	thinkd_log(LOG_ERR, "failed to allocate memory, exiting.");
+	exit(EXIT_FAILURE);
 }
 
 void *ec_malloc(size_t nbytes)
 {
-	return ec_allocated(malloc(nbytes));
+	void *ptr;
+
+	ptr = ec_allocated(malloc(nbytes));
+#if _DEBUG_LOG
+	thinkd_log(LOG_DEBUG, "malloc: address=%p; size=%zu", ptr, nbytes);
+#endif
+	return ptr;
 }
 
 void *ec_calloc(size_t nelems, size_t nbytes)
 {
-	return ec_allocated(calloc(nelems, nbytes));
+	void *ptr;
+	
+	ptr = ec_allocated(calloc(nelems, nbytes));
+#if _DEBUG_LOG
+	thinkd_log(LOG_DEBUG, "calloc: address=%p; nmem=%zu; size=%zu", ptr, nelems, nbytes); 
+#endif
+	return ptr;
 }
+
